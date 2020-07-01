bfb09dee11fdd4df4a57b2f90b1e612c3fa6cacd
tanmay <taghosh100gmail.com>
2020-07-01 14:42:17 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/ourproject/weatherscape/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ourproject.weatherscape
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //todo:2 Have to decide a theme for the app.
+      home: Scaffold(),//todo:1 add a new dart page for the starting screen
+    );
+  }
+}
